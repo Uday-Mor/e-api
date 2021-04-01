d3980450e4abe4b5d2c8aabbe56a71795ed3b248
nikunj vaghasiya <59605005+nikunjvaghasiya@users.noreply.github.com>
2021-04-01 16:46:01 -0700

    Migration created for products and reviews

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::apiResource('/products', 'ProductController');
 
-Route::group(['prefix'=>'products'], function(){
-	Route::apiResource('/{products}/reviews', 'ReviewController');
+Route::group(['prefix'=>'products'],function(){
+	Route::apiResource('/{product}/reviews','ReviewController');
 });
