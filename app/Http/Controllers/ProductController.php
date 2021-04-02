d328ae3b1ad6bb5451a499e04c50877eb680fa7a
nikunj vaghasiya <59605005+nikunjvaghasiya@users.noreply.github.com>
2021-04-02 10:48:23 -0700

    Product Collection Resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\ProductCollection;
 use App\Model\Product;
 use Illuminate\Http\Request;
 
@@ -10,7 +11,7 @@ class ProductController extends Controller
    
     public function index()
     {
-        return Product::all();
+        return ProductCollection::collection(Product::all());
     }
 
   
--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,6 +15,7 @@ class ProductResource extends Resource
             'price' => $this->price,
             'stock' => $this->stock ==  0 ? 'Out of Stock' : $this->stock,
             'discount' => $this->discount,
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price,2),
             'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(), 2) : 'No Rating Yet',
             'href' => [
                 'reviews' => route('reviews.index', $this->id)
