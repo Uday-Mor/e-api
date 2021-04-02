46a69f1f3d124e2f5c640540c110e17979d2da7d
nikunj vaghasiya <59605005+nikunjvaghasiya@users.noreply.github.com>
2021-04-01 17:27:18 -0700

    Creating Relationship

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Model;
-
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function reviews() {
+    	return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Model;
-
+use App\Model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public funciton product() {
+    	return $this->belongsTo(Product::class);
+    }
 }
