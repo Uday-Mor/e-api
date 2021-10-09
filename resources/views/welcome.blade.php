5fac133cf188da3620e47ddbe0a4c38e7207b7bc
Uday-Mor <90046260+Uday-Mor@users.noreply.github.com>
2021-10-09 10:34:17 +0530

    update home pagetitle
    update home page heading in welcome.blade.php file under resources folder.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,7 @@
         </style>
     </head>
     <body>
+    <h1>This is Book API</h1>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
